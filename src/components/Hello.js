efef8ec6f21bbb9eee489c02a0c295b0a78a7be8
oh <w.juthatip@gmail.com>
2017-01-14 15:21:02 +0700

    mock data to fetch data from server

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,37 @@
 import React , { Component } from 'react'
 import style from './Hello.scss'
+import axios from 'axios'
 
 class Hello extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    }
+  }
+  componentDidMount() {
+    axios.get(`http://localhost:3000/db`)
+     .then(res => {
+       const posts = res.data.jobs.map(obj => obj);
+       this.setState({ posts })
+     });
+  }
   render() {
     return (
-      <h1 className={style.title}>Hello Starter !!</h1>
+      <div>
+        <h1 className={style.title}>Hello Starter !!</h1>
+        {this.state.posts.map((post, i)=> {
+            return (
+              <div className={style.list} key={i}>
+                <a className={style.link} href={post.url}> {post.company_name}</a>
+                <p> {post.title} <span>- {post.term}</span></p>
+              </div>
+            )
+          })
+        }
+      </div>
     )
   }
 }
